42d98a742c25a36df3084d5b40fd91b13ffa6627
Jonny0430 <ahadjon697gmail.com>
2024-11-28 22:26:36 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -4,23 +4,21 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class MemberService {
-  constructor(
-    @InjectModel('Member') private readonly memberModel: Model<null>,
-  ) {}
+	constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
 
-  public async signup(): Promise<string> {
-    return 'updateMember executed!';
-  }
+	public async signup(): Promise<string> {
+		return 'updateMember executed!';
+	}
 
-  public async login(): Promise<string> {
-    return ' login executed!';
-  }
+	public async login(): Promise<string> {
+		return ' login executed!';
+	}
 
-  public async updateMember(): Promise<string> {
-    return 'updateMember executed!';
-  }
+	public async updateMember(): Promise<string> {
+		return 'updateMember executed!';
+	}
 
-  public async getMember(): Promise<string> {
-    return 'getMember executed!';
-  }
+	public async getMember(): Promise<string> {
+		return 'getMember executed!';
+	}
 }
