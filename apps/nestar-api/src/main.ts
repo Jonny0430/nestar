c729e38d93e488eca9ce408a69911ca868e085e5
Jonny0430 <ahadjon697gmail.com>
2024-12-10 13:14:05 +0900

    feat imagesUplods

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 	app.useGlobalInterceptors(new LoggingInterceptor());
 	app.enableCors({ origin: true, credentials: true });
 
-	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
+	app.use(graphqlUploadExpress({ maxFileSize: 1500000, maxFiles: 10 }));
 	app.use(`/uploads`, express.static(`./uploads`));
 
 	await app.listen(process.env.PORT_API ?? 3000);
